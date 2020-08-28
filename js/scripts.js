19774830ff531dc894439b92b5d458c330c06d98
JaredRamirez <me@jaredramirez.com>
2020-08-28 16:50:13 -0700

    site changes

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+if (typeof jQuery === "undefined") {
+    throw new Error("Bootstrap's JavaScript requires jQuery");
+  }
+
+  
+  
+  $(document).ready(function() { 
+  
+   
+  });//doc ready
+  
+$(window).load(function() {
+    var $grid = $('.tiles').masonry({
+    //columnWidth: 80
+    });
+});
+
+  
+  
+  
